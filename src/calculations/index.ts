a7e320874217d57c266b948502356edd2ce64f43
tonydawhale <tonydawhale@gmail.com>
2022-03-13 18:58:41 -0700

    awe

--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -1,7 +1,10 @@
-export { skillWeight } from "./calcSkillWeight"
-export { totalSkillWeight } from "./calcTotalSkillWeight"
-export { slayerWeight } from "./calcSlayerWeight"
-export { totalSlayerWeight } from "./calcTotalSlayerWeight"
-export { dungeonWeight } from "./calcDungeonWeight"
-export { totalDungeonWeight } from "./calcTotalDungeonWeight"
-export { totalWeight } from "./calcTotalWeight"
+// import { skillWeight } from "./calcSkillWeight"
+//
+//
+// export { skillWeight } from "./calcSkillWeight"
+// // export { totalSkillWeight } from "./calcTotalSkillWeight"
+// // export { slayerWeight } from "./calcSlayerWeight"
+// // export { totalSlayerWeight } from "./calcTotalSlayerWeight"
+// // export { dungeonWeight } from "./calcDungeonWeight"
+// // export { totalDungeonWeight } from "./calcTotalDungeonWeight"
+// // export { totalWeight } from "./calcTotalWeight"
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import {dungeonWeight, slayerWeight} from "./calculations";
-
+import {skillWeight} from "./calculations/calcSkillWeight";
+import { totalSkillWeight } from "./calculations/calcTotalSkillWeight"
 export * from "./types/parameters/parameterTypes"
-export * from "./calculations"
 
-let svenWeight = dungeonWeight("catacombs", 25000)
-console.log(svenWeight)
+export { skillWeight } from "./calculations/calcSkillWeight";
+export { totalSkillWeight } from "./calculations/calcTotalSkillWeight"
+export { slayerWeight } from "./calculations/calcSlayerWeight"
+export { totalSlayerWeight } from "./calculations/calcTotalSlayerWeight"
+export { dungeonWeight } from "./calculations/calcDungeonWeight"
+export { totalDungeonWeight } from "./calculations/calcTotalDungeonWeight"
+export { totalWeight } from "./calculations/calcTotalWeight"
